9a2c85072c72b4d48109ed8c24488a891ba66db3
Ibrohimjon707 <ibrohimjonathamjonov38@gmail.com>
2025-01-07 08:36:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let ism = prompt("Ismingizni kiriting:");
+console.log('Ism:', ism);
+let Familiyangiz = prompt("Familiyangizni kiriting:");
+console.log('Familiya:', Familiyangiz)
+let yosh = prompt("Yoshingizni kiriting:", 16);
+console.log('Yosh', yosh)
+let fan = prompt("Yoqtirgan faninigizni yozing")
+console.log('Yoqtirgan fan:', fan)
+let tuman = prompt("Yashayotgan tumaningizni kiriting")
+console.log('Yashash mazili:', tuman)
+let tasdiqlash = confirm("Malumotmotlarni kiritganingiz uchun tashakkur");
+console.log(tasdiqlash)
